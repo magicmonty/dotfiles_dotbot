183fde56d9415892004be2b2e487009989c8398e
Martin Gondermann <magicmonty@users.noreply.github.com>
2018-01-15 09:57:34 +0100

    Update init.rb

--- apps/sonic-pi/init.rb
+++ apps/sonic-pi/init.rb
@@ -1,21 +1,105 @@
 # Sonic Pi init file
 # Code in here will be evaluated on launch.
 
-def volca_fm(note, sustain: 0.25, vel: 50)
-  midi_cc 41, vel, port: "usb_uno_midi_interface", channel: 1
-  midi note, sustain: sustain, port: "usb_uno_midi_interface", channel: 1
+$root = :A
+$octave = 4
+$tonic = :minor
+$progression = :I
+
+def cur_root
+  note $root, octave: $octave
 end
 
-def volca_bass(note, sustain: 0.25, vel: 128)
-  midi note, sustain: sustain, vel: vel, port: "usb_uno_midi_interface", channel: 2
+def cur_scale_bare (num_octaves: 1)
+  scale $tonic, num_octaves: num_octaves
 end
 
-def c1(note, sustain: 1.0, vel: 128)
-  midi note, sustain: sustain, vel: vel, port: "circuit", channel: 1
+def cur_scale (num_octaves: 1)
+  scale cur_root, $tonic, num_octaves: num_octaves
 end
 
-def c2(note, sustain: 1.0, vel: 128)
-  midi note, sustain: sustain, vel: vel, port: "circuit", channel: 2
+$modes = { 
+  :major => [ :major7, :minor7, :minor7, :major7, "7", :minor7 ],
+  :minor => [ :minor7, "m7-5", :major7, :minor7, :minor7, :major7, "7" ]
+}
+
+$scales = {
+  :C => [ :C, :D, :Eb, :F, :G, :Ab, :Bb ],
+  :Db => [ :Dd, :Eb, :E, :Gb, :Ab, :A, :B ],
+  :D => [ :D, :E, :F, :G, :A, :Bb, :C ],
+  :Eb => [ :Eb, :F, :Gb, :Ab, :Bb, :B, :Db ],
+  :E => [ :E, :Gb, :G, :A, :B, :C, :D ],
+  :F => [ :F, :G, :Ab, :Bb, :C, :Db, :Eb ],
+  :Gb => [ :Gb, :Ab, :A, :B, :Db, :D, :E ],
+  :G => [ :G, :A, :Bb, :C, :D, :Eb, :F ],
+  :Ab => [ :Ab, :Bb, :B, :Db, :Eb, :E, :Gb ],
+  :A => [ :A, :B, :C, :D, :E, :F, :G ],
+  :Bb => [ :Bb, :C, :Db, :Eb, :F, :Gb, :Ab ],
+  :B => [ :B, :Db, :D, :E, :Gb, :G, :A ]
+}
+
+def get_progression_num (sym)
+  case sym
+  when :I, :i
+    0
+  when :II, :ii
+    1
+  when :III, :iii
+    2
+  when :IV, :iv
+    3
+  when :V, :v
+    4
+  when :VI, :vi
+    5
+  when :VII, :vii
+    6
+  else 
+    (sym - 1) % 7
+  end
+end
+
+def cur_progression (num: $progression)
+  index = get_progression_num num
+  if $modes.has_key?($tonic) && (index < $modes[$tonic].length)
+    pitch_class = (note_info cur_root).pitch_class
+    if $scales.has_key?(pitch_class)
+      [$scales[pitch_class][index], $modes[$tonic][index]]
+    else
+      [$root, $tonic]
+    end
+  else
+    [$root, $tonic]
+  end
+end
+
+$markov =
+  {
+    :major => {
+      :I => [ :II, :IV, :VI ],
+      :II => [ :V ],
+      :IV => [ :V ],
+      :V => [ :I ],
+      :VI => [ :IV ]
+    },
+    :minor => {
+      :I => [ :II, :IV, :VI ],
+      :II => [ :V ],
+      :III => [ :VII ],
+      :IV => [ :VII, :V ],
+      :V => [ :I ],
+      :VI => [ :III, :VII ],
+      :VII => [ :I ]
+    }
+  }
+
+def progress_markov
+  if $markov.has_key?($tonic)
+    chain = $markov[$tonic]
+    if chain.has_key?($progression)
+      $progression = chain[$progression].choose
+    end
+  end
 end
 
 def cosr(center, range, cycle)
@@ -68,5 +152,19 @@ def unmute
   set_mixer_control! amp: 1.0
 end
 
-$root = :a4
-$scl = (scale :minor)
+def volca_fm(note, sustain: 0.25, vel: 50)
+  midi_cc 41, vel, port: "usb_uno_midi_interface", channel: 1
+  midi note, sustain: sustain, port: "usb_uno_midi_interface", channel: 1
+end
+
+def volca_bass(note, sustain: 0.25, vel: 128)
+  midi note, sustain: sustain, vel: vel, port: "usb_uno_midi_interface", channel: 2
+end
+
+def c1(note, sustain: 1.0, vel: 128)
+  midi note, sustain: sustain, vel: vel, port: "circuit", channel: 1
+end
+
+def c2(note, sustain: 1.0, vel: 128)
+  midi note, sustain: sustain, vel: vel, port: "circuit", channel: 2
+end
