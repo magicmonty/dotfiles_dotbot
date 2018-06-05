8d257be2e5220357374ebb75acc19b3ecf16a27a
Martin Gondermann <magicmonty@pagansoft.de>
2018-06-05 21:35:52 +0200

    Add qutebrowser config, add addpocket helper

---
+++ apps/qutebrowser/config.py
@@ -0,0 +1,24 @@
+config.source('shortcuts.py')
+
+# open current URL in MPV
+config.bind("m", "spawn ~/bin/umpv '{url}'")
+
+# open link in MPV
+config.bind(";M", "hint links spawn ~/bin/umpv '{hint-url}'")
+
+# open link
+config.bind(",o", "hint links fill :open {hint-url}")
+
+# open link in new tab
+config.bind(";O", "hint links fill :open -t -r {hint-url}")
+
+# Yank link
+config.bind(",y", "hint links yank")
+
+# Add to Pocket
+config.bind(",p", "hint links spawn ~/bin/addpocket {hint-url}")
+config.bind("P", "spawn ~/bin/addpocket {url} ")
+config.bind("gp", ":open https://getpocket.com/a/queue")
+
+config.load_autoconfig()
+
