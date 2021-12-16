371b964f2340ddefa7b4909eab66b3e51508d197
Martin Gondermann <magicmonty@pagansoft.de>
2021-12-16 22:51:03 +0100

    Add leftwm rice

---
+++ windowmanager/leftwm/themes/nightfox/rofi/powermenu.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+THEME="$HOME/.config/leftwm/themes/current/rofi/powermenu.rasi"
+
+rofi_command="rofi -no-config -theme $THEME"
+
+# Options
+shutdown="Shutdown"
+reboot="Restart"
+lock="Lock"
+suspend="Suspend"
+logout="Logout"
+
+# Variable passed to rofi
+options="$lock\n$suspend\n$logout\n$reboot\n$shutdown"
+
+chosen="$(echo -e "$options" | $rofi_command -dmenu -selected-row 0)"
+case $chosen in
+    $shutdown)
+      systemctl poweroff
+        ;;
+    $reboot)
+		  systemctl reboot
+        ;;
+    $lock)
+			betterlockscreen -l
+        ;;
+    $suspend)
+		  systemctl suspend
+        ;;
+    $logout)
+      $HOME/.config/leftwm/themes/current/down && pkill leftwm
+        ;;
+esac
