3cf3848f12d782d97534aab079af31871e364a43
Martin Gondermann <magicmonty@pagansoft.de>
2021-12-17 15:30:35 +0100

    Update screen layouts

--- screenlayout/single.sh
+++ screenlayout/single.sh
@@ -4,11 +4,11 @@ MONITORS=$(xrandr --listmonitors | grep -e DP2-1 -e DP2-3 | wc -l)
 
 if [[ $MONITORS -eq 2 ]]; then
   xrandr \
-    --output eDP1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal \
+    --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal \
     --output DP2-3 --mode 1920x1080 --pos 0x0 --rotate normal \
-    --output DP2-1 --mode 1920x1080 --pos 3840x0 --rotate normal
+    --output DP2-1 --mode 1920x1080 --pos 0x0 --rotate normal
 fi
-# nitrogen --restore > /dev/null 2>&1
+
 if [[ -e $XDG_RUNTIME_DIR/leftwm/commands.pipe ]]; then
   echo "SoftReload" >> $XDG_RUNTIME_DIR/leftwm/commands.pipe
 fi
